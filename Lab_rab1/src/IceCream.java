05033e3811581c182840a7bb0d38af390e619a63
Alex <rct.mancevic@bsu.by>
2021-10-28 22:51:31 +0300

    IceCream

--- Lab_rab1/src/IceCream.java
+++ Lab_rab1/src/IceCream.java
@@ -2,6 +2,8 @@ public class IceCream extends Food
 {
 
 
+    private String syrup;
+
     public IceCream ()
     {
         super ("IceCream");
@@ -14,6 +16,24 @@ public class IceCream extends Food
 
        System.out.println( this + "Съедено");
 
+    }
+
+    public String  get_syrup()
+    {
+        return syrup;
+
+    }
+
+    public void set_syrup (String syrup1)
+    {
+        syrup = syrup1;
+
+    }
+
+    public boolean equals(Object arg0) {
+        if (!(arg0 instanceof IceCream)) return false;
+        if (syrup==null || ((IceCream)arg0).syrup==null) return false;
+        return syrup.equals(((IceCream)arg0).syrup);
     }
 
 }
