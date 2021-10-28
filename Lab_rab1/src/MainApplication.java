44acd9de4710aa024a03a772a78397f67c168927
Alex <rct.mancevic@bsu.by>
2021-10-28 11:26:17 +0300

    Hi

--- Lab_rab1/src/MainApplication.java
+++ Lab_rab1/src/MainApplication.java
@@ -2,9 +2,9 @@ public class MainApplication {
 
 public static void main (String [] args)
 {
-System.out.println( "hello" + "world" );
+System.out.println( "Hello " + "world" );
 
-
+    System.out.println( "Hello " + "world" );
 }
 
 
